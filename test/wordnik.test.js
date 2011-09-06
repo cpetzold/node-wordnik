33533916fa113f11117f0cf5c926b6d3b0517d6b
Conner Petzold <cpetzold@gmail.com>
2011-09-05 19:22:04 -0700

    added tests for word calls, expresso dev dep, .gitignore

---
+++ test/wordnik.test.js
@@ -0,0 +1,107 @@
+var assert = require('assert')
+  , key = require('./key')
+  , Wordnik = require('../');
+
+var options = { api_key: key };
+
+module.exports = {
+  
+  'Wordnik#constructor no key': function() {
+    try {
+      var wn = new Wordnik();
+    } catch(e) {
+      assert.isNotNull(e);
+    }
+  },
+  
+  'Wordnik#word': function() {
+    var wn = new Wordnik(options);
+    
+    wn.word('test', function(e, word) {
+      assert.isNull(e);
+      assert.isNotNull(word);
+    });
+  },
+  
+  'Wordnik#examples': function() {
+    var wn = new Wordnik(options);
+    
+    wn.examples('test', function(e, examples) {
+      assert.isNull(e);
+      assert.isNotNull(examples);
+    });
+  },
+  
+  'Wordnik#definitions': function() {
+    var wn = new Wordnik(options);
+    
+    wn.definitions('test', function(e, defs) {
+      assert.isNull(e);
+      assert.isNotNull(defs);
+    });
+  },
+  
+  'Wordnik#frequency': function() {
+    var wn = new Wordnik(options);
+    
+    wn.frequency('test', function(e, freq) {
+      assert.isNull(e);
+      assert.isNotNull(freq);
+    });
+  },
+  
+  'Wordnik#topExample': function() {
+    var wn = new Wordnik(options);
+    
+    wn.topExample('test', function(e, example) {
+      assert.isNull(e);
+      assert.isNotNull(example);
+    });
+  },
+  
+  'Wordnik#related': function() {
+    var wn = new Wordnik(options);
+    
+    wn.related('test', function(e, related) {
+      assert.isNull(e);
+      assert.isNotNull(related);
+    });
+  },
+  
+  'Wordnik#phrases': function() {
+    var wn = new Wordnik(options);
+    
+    wn.phrases('test', function(e, related) {
+      assert.isNull(e);
+      assert.isNotNull(related);
+    });
+  },
+  
+  'Wordnik#hyphenation': function() {
+    var wn = new Wordnik(options);
+    
+    wn.hyphenation('test', function(e, hyph) {
+      assert.isNull(e);
+      assert.isNotNull(hyph);
+    });
+  },
+  
+  'Wordnik#pronunciations': function() {
+    var wn = new Wordnik(options);
+    
+    wn.pronunciations('test', function(e, pron) {
+      assert.isNull(e);
+      assert.isNotNull(pron);
+    });
+  },
+  
+  'Wordnik#audio': function() {
+    var wn = new Wordnik(options);
+    
+    wn.audio('word', function(e, audio) {
+      assert.isNull(e);
+      assert.isNotNull(audio);
+    });
+  }
+
+};
